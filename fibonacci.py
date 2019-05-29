07a042885410c7c20418ac73f33d5a248f0f2454
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-05-29 08:25:04 +0100

    added fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -11,17 +11,29 @@ def fibonacci(n):
     return sequence
 
 
-def output(f):
+def output_all(f):
     print("n", "Fibonacci Sequence")
     for i in range(0, len(f)):
         print(i+1, f[i])
 
 
+def nth_term(f,n):
+    try:
+        result = f[n-1]
+    except Exception as e:
+        print("Error: {0}".format(e))
+    else:
+        return result
+
+
 def main():
     start_time = time.time()
-    f = fibonacci(1000)
-    output(f)
-    print("run time: " + str(time.time() - start_time))
+    f = fibonacci(100)
+    run_time = str(time.time() - start_time)
+    output_all(f)
+    n = 99
+    print("The {0}th term is {1}".format(n, nth_term(f, n)))
+    print("run time: {0}".format(run_time))
 
 
 if __name__ == "__main__":
