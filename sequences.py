0f8b2022a914c5da29de4208a2f87d66b0a7eec6
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-05-30 21:39:14 +0100

    update

--- sequences.py
+++ sequences.py
@@ -1,47 +1,24 @@
 # sequences.py
 
-error_str = "Error: {0}"
-run_time_str = "{0} numbers run time: {1} seconds"
-nth_term_str = "The {0}th term of the {1} numbers is {2}"
-filename_str = "{0}_{1}.txt"
 
-
-def sequence(n, first_term, second_term):
+def sequence(n, first_term, second_term, multiplier_first_term=1, multiplier_second_term=1):
     # generate sequence to the (n)th term
     seq = []
     # Set the first two terms
     a, b = first_term, second_term
+
+    # Set the multipliers for the terms (default = 1)
+    c, d = multiplier_first_term, multiplier_second_term
+
+    # Add the first term to the sequence result
     seq.append(a)
+
     # Generate the sequence by adding the two previous terms
     while len(seq) < n+1:
         seq.append(b)
-        a, b = b, a+b
+        a, b = b, (c*a)+(d*b)
+
+    # Return the sequence of n terms
     return seq
 
 
-def output_all_to_screen(seq, sequence_str):
-    print("\n{0} Sequence".format(sequence_str))
-    print("n", "Sequence")
-    for i in range(0, len(seq)):
-        print("{0} {1}".format(i, seq[i]))
-
-
-def output_all_to_file(seq, filename):
-    try:
-        with open(filename, "w") as file:
-            for i in range(0, len(seq)):
-                seq_str = "{0} {1}\n".format(i, seq[i])
-                file.write(seq_str)
-    except IOError as e:
-        print(error_str.format(e))
-    except TypeError as e:
-        print(error_str.format(e))
-
-
-def nth_term(seq, n):
-    try:
-        result = seq[n]
-    except IndexError as e:
-        print(error_str.format(e))
-    else:
-        return result
