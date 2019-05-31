32602b227a5e09b870131bef5151af6db3d17100
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-05-31 08:16:08 +0100

    updated comments

--- Sequences/sequences.py
+++ Sequences/sequences.py
@@ -1,9 +1,15 @@
 # sequences.py
 
 
-def sequence(n, first_term, second_term, multiplier_first_term=1, multiplier_second_term=1, constant_factor=0):
-    # generate sequence to the (n)th term
+def sequence(n, first_term,
+             second_term,
+             multiplier_first_term=1,
+             multiplier_second_term=1,
+             constant_factor=0):
+
+    # Create an empty list to hold the results
     seq = []
+
     # Set the first two terms
     a, b = first_term, second_term
 
@@ -16,9 +22,11 @@ def sequence(n, first_term, second_term, multiplier_first_term=1, multiplier_sec
     # Add the first term to the sequence result
     seq.append(a)
 
-    # Generate the sequence by adding the two previous terms
+    # Generate the sequence until the length of the sequence is n terms
     while len(seq) < n+1:
+        # add b to the list
         seq.append(b)
+        # new a = old b, new b = calculated term
         a, b = b, (c*a)+(d*b) + e
 
     # Return the sequence of n terms
