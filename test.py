fa58c3a69962ec38083854b91923e87ec12f4f06
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-05-31 07:55:54 +0100

    added Mersenne sequence

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from Sequences.fibonacci import fibonacci
 from Sequences.lucas import lucas
+from Sequences.mersenne import mersenne
 
 
 def sequence_test():
     fib = fibonacci(100)
     luc = lucas(100)
+    mer = mersenne(100)
 
     print("Fibonacci Numbers:\n ", fib)
     print("Lucas Numbers:\n", luc)
+    print("Mersenne Numbers:\n", mer)
 
 
 def main():
