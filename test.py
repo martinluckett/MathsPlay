586eb6b4dd4ed9d5ff96dfddf839387bb51325d3
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-05-31 07:07:02 +0100

    update test

--- test.py
+++ test.py
@@ -2,12 +2,16 @@ from Sequences.fibonacci import fibonacci
 from Sequences.lucas import lucas
 
 
-def main():
+def sequence_test():
     fib = fibonacci(100)
     luc = lucas(100)
 
     print("Fibonacci Numbers:\n ", fib)
     print("Lucas Numbers:\n", luc)
+
+
+def main():
+    sequence_test()
 
 
 if __name__ == "__main__":
