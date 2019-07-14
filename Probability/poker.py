b2cfa48022f858ac17c8a6ec96348ebad0e75499
Martin Luckett <16232159+martinluckett@users.noreply.github.com>
2019-07-14 09:48:54 +0100

    added probability and poker files

---
+++ Probability/poker.py
@@ -0,0 +1,14 @@
+# poker.py
+#
+#  Looking at various mathematical aspects of playing poker
+#
+# https://github.com/martinluckett/MathsPlay
+# Martin Luckett 2019
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
